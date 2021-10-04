9b46680cd72bc47d59c70380976332dfa9e6ba83
Nathan-Thibault <nathan.thibault@utt.fr>
2021-10-04 16:02:40 +0200

    feat: add InputListener
    Constantly listens for an entry by the user

--- src/fr/utt/lo02/witchhunt/WitchHunt.java
+++ src/fr/utt/lo02/witchhunt/WitchHunt.java
@@ -1,7 +1,15 @@
 package fr.utt.lo02.witchhunt;
 
-public class WitchHunt {
-    public static void main(String[] args){
+import fr.utt.lo02.witchhunt.cli.InputListener;
 
+public class WitchHunt {
+
+    public static void main(String[] args){
+        InputListener inputListener = new InputListener(System.in);
+        inputListener.start();
+    }
+
+    public static void echo(String str){
+        System.out.println("echo-> " + str);
     }
 }
