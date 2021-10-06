235fbaa5090239937ccd874d148097a97214f0a2
Nathan <nathan.thibault@utt.fr>
2021-10-06 19:14:46 +0200

    feat: add a way to run commands
    - Renamed InputListener to CommandListener witch makes more sense
    - Added readInput to get a command and its arguments from the input string. Then execute it by getting the corresponding Command class extending the AbstractCommand clas

--- src/fr/utt/lo02/witchhunt/WitchHunt.java
+++ src/fr/utt/lo02/witchhunt/WitchHunt.java
@@ -1,15 +1,11 @@
 package fr.utt.lo02.witchhunt;
 
-import fr.utt.lo02.witchhunt.cli.InputListener;
+import fr.utt.lo02.witchhunt.cli.CommandListener;
 
 public class WitchHunt {
 
     public static void main(String[] args){
-        InputListener inputListener = new InputListener(System.in);
-        inputListener.start();
-    }
-
-    public static void echo(String str){
-        System.out.println("echo-> " + str);
+        CommandListener commandListener = new CommandListener(System.in);
+        commandListener.start();
     }
 }
