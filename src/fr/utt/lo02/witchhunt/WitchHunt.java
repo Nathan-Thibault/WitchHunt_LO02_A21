55c8fd720e59657688c761722c33abf6be258f60
Nathan <nathan.thibault@utt.fr>
2021-10-08 18:57:26 +0200

    fix: color output only if not on windows console

--- src/fr/utt/lo02/witchhunt/WitchHunt.java
+++ src/fr/utt/lo02/witchhunt/WitchHunt.java
@@ -1,10 +1,13 @@
 package fr.utt.lo02.witchhunt;
 
 import fr.utt.lo02.witchhunt.cli.CommandListener;
+import fr.utt.lo02.witchhunt.cli.Utils;
 
 public class WitchHunt {
 
     public static void main(String[] args){
+        Utils.setRunningOnWindows();
+
         CommandListener commandListener = new CommandListener(System.in);
         commandListener.start();
     }
--- src/fr/utt/lo02/witchhunt/cli/Utils.java
+++ src/fr/utt/lo02/witchhunt/cli/Utils.java
@@ -1,29 +1,53 @@
 package fr.utt.lo02.witchhunt.cli;
 
+import java.io.IOException;
+
 public class Utils {
+
+    private static boolean windowsConsole;
+
+    public static void setRunningOnWindows(){
+        windowsConsole = (System.console() != null && System.getProperty("os.name").contains("Windows"));
+    }
+
     public static void resetScreen(){
-        /*
-        \033[H moves the cursor at the top left corner of the screen or console.
-        \033[2J clears the screen from the cursor to the end of the screen.
-         */
-        System.out.print("\033[H\033[2J");
-        System.out.flush();
+        if(windowsConsole){
+            try {
+                //run cls command
+                new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
+            } catch (IOException | InterruptedException ignored){}
+        } else {
+            /*
+            \033[H moves the cursor at the top left corner of the screen or console.
+            \033[2J clears the screen from the cursor to the end of the screen.
+             */
+            System.out.print("\033[H\033[2J");
+            System.out.flush();
+        }
     }
 
     public static void coloredOutput(String str, ANSIColor color){
-        System.out.println(color.toString() + str + ANSIColor.RESET);
+        coloredOutput(str, new ANSIColor[]{color});
     }
 
     public static void coloredOutput(String str, ANSIColor[] colors){
-        String output = "";
-        for (ANSIColor color : colors) {
-            output = output.concat(color.toString());
+        if(windowsConsole){
+            System.out.println(str);
+        } else {
+            String output = "";
+            for (ANSIColor color : colors) {
+                output = output.concat(color.toString());
+            }
+            output = output + str + ANSIColor.RESET;
+            System.out.println(output);
         }
-        output = output + str + ANSIColor.RESET.toString();
-        System.out.println(output);
     }
 
     public static void errorOutput(String str){
-        System.out.println(ANSIColor.WHITE_BACKGROUND.toString() + ANSIColor.RED_BOLD.toString() + str + ANSIColor.RESET.toString());
+        if(windowsConsole){
+            System.out.println("Erreur: " + str);
+        } else {
+            System.out.println(ANSIColor.WHITE_BACKGROUND.toString() + ANSIColor.RED_BOLD + "Erreur: " + str + ANSIColor.RESET);
+        }
     }
 }
