72b052dad4c5cb329543c862bd499a767b758ed1
Nathan <nathan.thibault@utt.fr>
2021-10-09 16:50:00 +0200

    feat: verify that constructor argument isn't null

--- src/fr/utt/lo02/witchhunt/card/IdentityCard.java
+++ src/fr/utt/lo02/witchhunt/card/IdentityCard.java
@@ -2,13 +2,15 @@ package fr.utt.lo02.witchhunt.card;
 
 import fr.utt.lo02.witchhunt.Identity;
 
+import java.util.Objects;
+
 public final class IdentityCard extends Card{
 
     private final Identity identity;
 
     public IdentityCard(Identity identity){
         super(false);
-        this.identity = identity;
+        this.identity = Objects.requireNonNull(identity, "IdentityCard constructor: identity can't be null");
     }
 
     @Override
