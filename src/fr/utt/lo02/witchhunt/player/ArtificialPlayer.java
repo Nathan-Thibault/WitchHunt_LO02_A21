8c6b95cbf46930fbeb69f17fc6b220835a0d5650
Nathan-Thibault <nathan.thibault@utt.fr>
2021-10-11 15:08:38 +0200

    feat: add ArtificialPlayer

---
+++ src/fr/utt/lo02/witchhunt/player/ArtificialPlayer.java
@@ -0,0 +1,39 @@
+package fr.utt.lo02.witchhunt.player;
+
+import fr.utt.lo02.witchhunt.Identity;
+import fr.utt.lo02.witchhunt.card.RumourCard;
+import fr.utt.lo02.witchhunt.player.strategy.IdentityStrategy;
+import fr.utt.lo02.witchhunt.player.strategy.RespondStrategy;
+import fr.utt.lo02.witchhunt.player.strategy.TurnStrategy;
+
+import java.util.ArrayList;
+import java.util.Objects;
+
+public class ArtificialPlayer extends Player{
+
+    private TurnStrategy turnStrategy;
+    private RespondStrategy respondStrategy;
+    private IdentityStrategy identityStrategy;
+
+    public ArtificialPlayer(ArrayList<RumourCard> hand, TurnStrategy turnStrategy, RespondStrategy respondStrategy, IdentityStrategy identityStrategy){
+        super(hand);
+        this.turnStrategy = Objects.requireNonNull(turnStrategy, "ArtificialPlayer constructor: turnStrategy can't be null");
+        this.respondStrategy = Objects.requireNonNull(respondStrategy, "ArtificialPlayer constructor: respondStrategy can't be null");
+        this.identityStrategy = Objects.requireNonNull(identityStrategy, "ArtificialPlayer constructor: identityStrategy can't be null");
+    }
+
+    @Override
+    public Player accuse() {
+        return null;
+    }
+
+    @Override
+    public RumourCard selectCardFromHand() {
+        return null;
+    }
+
+    @Override
+    public void chooseIdentity(Identity identity) {
+
+    }
+}
