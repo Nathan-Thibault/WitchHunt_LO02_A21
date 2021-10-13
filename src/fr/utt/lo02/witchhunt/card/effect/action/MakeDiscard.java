2f54386795f980e8b349c956c72ee7674c810198
Nathan <nathan.thibault@utt.fr>
2021-10-13 21:15:59 +0200

    feat: add action MakeDiscard

---
+++ src/fr/utt/lo02/witchhunt/card/effect/action/MakeDiscard.java
@@ -0,0 +1,30 @@
+package fr.utt.lo02.witchhunt.card.effect.action;
+
+import fr.utt.lo02.witchhunt.Utils;
+import fr.utt.lo02.witchhunt.card.CardManager;
+import fr.utt.lo02.witchhunt.player.Player;
+import fr.utt.lo02.witchhunt.player.PlayerManager;
+
+import java.util.ArrayList;
+
+public class MakeDiscard extends Action{
+
+    public MakeDiscard(){
+        super("The player who accused you discards\na random card from their hand.");
+    }
+
+    @Override
+    public boolean execute(Player caller) {
+        PlayerManager pManager = PlayerManager.getInstance();
+        ArrayList<String> playersWithCards = pManager.getPlayersWithCards();
+        if(playersWithCards.isEmpty()){
+            //TODO: can't execute this action -> can't play card
+            return false;
+        } else {
+            String target = caller.choosePlayerFrom(playersWithCards);
+            //discard random card from target's hand
+            CardManager.getInstance().discard(Utils.randomFromList(pManager.getByName(target).getHand()));
+            return true;
+        }
+    }
+}
