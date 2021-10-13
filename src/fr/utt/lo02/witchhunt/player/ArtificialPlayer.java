e9ec0f083efc2d084fbe940ca042a8d1dcc855fd
Nathan <nathan.thibault@utt.fr>
2021-10-13 20:43:31 +0200

    chore: remove unused imports

--- src/fr/utt/lo02/witchhunt/player/ArtificialPlayer.java
+++ src/fr/utt/lo02/witchhunt/player/ArtificialPlayer.java
@@ -1,15 +1,12 @@
 package fr.utt.lo02.witchhunt.player;
 
 import fr.utt.lo02.witchhunt.Utils;
-import fr.utt.lo02.witchhunt.card.CardManager;
-import fr.utt.lo02.witchhunt.card.RumourCard;
 import fr.utt.lo02.witchhunt.player.strategy.identity.IdentityStrategy;
 import fr.utt.lo02.witchhunt.player.strategy.respond.RespondStrategy;
 import fr.utt.lo02.witchhunt.player.strategy.turn.TurnStrategy;
 
 import java.util.ArrayList;
 import java.util.Objects;
-import java.util.Random;
 
 public final class ArtificialPlayer extends Player{
 
