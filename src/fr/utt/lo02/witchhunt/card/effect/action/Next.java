14070a6aa28893a746cbdbf4ef5f5c58c1a71837
Nathan <nathan.thibault@utt.fr>
2021-10-16 15:26:09 +0200

    feat: add action Next

---
+++ src/fr/utt/lo02/witchhunt/card/effect/action/Next.java
@@ -0,0 +1,20 @@
+package fr.utt.lo02.witchhunt.card.effect.action;
+
+import fr.utt.lo02.witchhunt.RoundManager;
+import fr.utt.lo02.witchhunt.player.Player;
+import fr.utt.lo02.witchhunt.player.PlayerManager;
+
+public final class Next extends Action{
+
+    public Next(){
+        super("Choose next player.");
+    }
+
+    @Override
+    public boolean execute(Player caller) {
+        String target = caller.choosePlayerFrom(PlayerManager.getInstance().getInGamePlayers());
+        RoundManager.getInstance().setIndexAtPlayer(target);
+        RoundManager.getInstance().next();
+        return true;
+    }
+}
