309feceb12d38a8c2833ad1cbf90bd6a9586fa0c
Nathan <nathan.thibault@utt.fr>
2021-10-16 16:08:32 +0200

    feat: add Look action

---
+++ src/fr/utt/lo02/witchhunt/card/effect/action/Look.java
@@ -0,0 +1,20 @@
+package fr.utt.lo02.witchhunt.card.effect.action;
+
+import fr.utt.lo02.witchhunt.Identity;
+import fr.utt.lo02.witchhunt.card.effect.CardEffect;
+import fr.utt.lo02.witchhunt.player.Player;
+import fr.utt.lo02.witchhunt.player.PlayerManager;
+
+public final class Look extends Action{
+
+    public Look(){
+        super("Before their turn, secretly\nlook at their identity");
+    }
+
+    @Override
+    public boolean execute(Player caller, CardEffect effect) {
+        Identity targetIdentity = PlayerManager.getInstance().getByName(effect.getTarget()).getIdentityCard().getIdentity();
+        //TODO : tell caller the identity of it's target
+        return false;
+    }
+}
