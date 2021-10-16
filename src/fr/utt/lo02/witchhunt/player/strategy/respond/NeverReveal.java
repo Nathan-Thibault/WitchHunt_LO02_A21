1a021751cd3eb40cd5b89f4b774b7c865a70af3d
Nathan <nathan.thibault@utt.fr>
2021-10-16 20:36:26 +0200

    feat: make tryNotToReveal method logic

--- src/fr/utt/lo02/witchhunt/player/strategy/respond/NeverReveal.java
+++ src/fr/utt/lo02/witchhunt/player/strategy/respond/NeverReveal.java
@@ -1,6 +1,10 @@
 package fr.utt.lo02.witchhunt.player.strategy.respond;
 
+import fr.utt.lo02.witchhunt.card.CardManager;
+import fr.utt.lo02.witchhunt.card.effect.EffectType;
 import fr.utt.lo02.witchhunt.player.ArtificialPlayer;
+
+import java.util.ArrayList;
 
 public final class NeverReveal implements RespondStrategy{
     @Override
@@ -9,6 +13,19 @@ public final class NeverReveal implements RespondStrategy{
     }
 
     public static void tryNotToReveal(ArtificialPlayer caller){
-        //TODO play witch effect if possible otherwise reveal
+        CardManager cManager = CardManager.getInstance();
+
+        ArrayList<String> cards = caller.getHand();
+        String card = caller.chooseCardFrom(cards);
+
+        //try to play witch effect of every card in hand until there's one that works
+        while (!cards.isEmpty()){
+            if(cManager.getByName(card).playEffect(EffectType.WITCH, caller)){
+                return;
+            } else {
+                cards.remove(card);
+                card = caller.chooseCardFrom(cards);
+            }
+        }
     }
 }
