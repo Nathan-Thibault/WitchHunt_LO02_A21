dcd52871b7fdf0ddbd0ffe8c4fb6dd3e2269fbb3
Nathan <nathan.thibault@utt.fr>
2021-10-16 16:25:21 +0200

    feat: add RandomlyTakeCardFrom action

---
+++ src/fr/utt/lo02/witchhunt/card/effect/action/RandomlyTakeCardFrom.java
@@ -0,0 +1,23 @@
+package fr.utt.lo02.witchhunt.card.effect.action;
+
+import fr.utt.lo02.witchhunt.Utils;
+import fr.utt.lo02.witchhunt.card.effect.CardEffect;
+import fr.utt.lo02.witchhunt.player.Player;
+import fr.utt.lo02.witchhunt.player.PlayerManager;
+
+public final class RandomlyTakeCardFrom extends Action{
+
+    public RandomlyTakeCardFrom(){
+        super("Before their turn, take a random card\nfrom their hand and add it to your hand.");
+    }
+
+    @Override
+    public boolean execute(Player caller, CardEffect effect) {
+        Player target = PlayerManager.getInstance().getByName(effect.getTarget());
+
+        String card = Utils.randomFromList(target.getHand());
+        caller.getHand().add(card);
+
+        return true;
+    }
+}
