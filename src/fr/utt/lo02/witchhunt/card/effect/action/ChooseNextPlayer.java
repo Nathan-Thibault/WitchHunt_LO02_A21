29bb60f27c8b06e8e715ae883e11e134c83920d7
Nathan <nathan.thibault@utt.fr>
2021-10-16 16:23:24 +0200

    refactor: rename Next to ChooseNextPlayer and add way to choose players with cards

--- src/fr/utt/lo02/witchhunt/card/effect/action/ChooseNextPlayer.java
+++ src/fr/utt/lo02/witchhunt/card/effect/action/ChooseNextPlayer.java
@@ -5,15 +5,26 @@ import fr.utt.lo02.witchhunt.card.effect.CardEffect;
 import fr.utt.lo02.witchhunt.player.Player;
 import fr.utt.lo02.witchhunt.player.PlayerManager;
 
-public final class Next extends Action{
+public final class ChooseNextPlayer extends Action{
 
-    public Next(){
+    private final boolean mustHaveCards;
+
+    public ChooseNextPlayer(boolean mustHaveCards){
         super("Choose next player.");
+
+        this.mustHaveCards = mustHaveCards;
     }
 
     @Override
     public boolean execute(Player caller, CardEffect effect) {
-        String target = caller.choosePlayerFrom(PlayerManager.getInstance().getInGamePlayers());
+        PlayerManager pManager = PlayerManager.getInstance();
+
+        String target;
+        if(mustHaveCards) {
+            target = caller.choosePlayerFrom(pManager.getPlayersWithCards());
+        } else {
+            target = caller.choosePlayerFrom(pManager.getInGamePlayers());
+        }
 
         effect.setTarget(target);
         RoundManager.getInstance().setIndexAtPlayer(target);
