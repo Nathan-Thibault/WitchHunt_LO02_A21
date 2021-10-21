6a9a691f6a41513acdedfd13021bef3bf1d1d135
Nathan-Thibault <nathan.thibault@utt.fr>
2021-10-21 17:09:08 +0200

    feat: creation of artificial players in game creating process

--- src/fr/utt/lo02/witchhunt/WitchHunt.java
+++ src/fr/utt/lo02/witchhunt/WitchHunt.java
@@ -1,6 +1,9 @@
 package fr.utt.lo02.witchhunt;
 
 import fr.utt.lo02.witchhunt.player.PlayerManager;
+import fr.utt.lo02.witchhunt.player.strategy.identity.IdentityStrategy;
+import fr.utt.lo02.witchhunt.player.strategy.respond.RespondStrategy;
+import fr.utt.lo02.witchhunt.player.strategy.turn.TurnStrategy;
 
 import java.io.IOException;
 import java.util.NoSuchElementException;
@@ -53,7 +56,7 @@ public class WitchHunt {
         PlayerManager pManager = PlayerManager.getInstance();
 
         for (int i = 1; i <= p; i++) {
-            System.out.println("Enter the name of the player \"".concat(Integer.toString(i)).concat("\" :"));
+            System.out.println("Enter the name of the player ".concat(Integer.toString(i)).concat(" :"));
             try {
                 pManager.addPhysicalPlayer(sc.nextLine());
                 System.out.println("Player successfully added.");
@@ -62,9 +65,17 @@ public class WitchHunt {
                 i--;
             }
         }
-        sc.close();
 
-        //TODO : create artificial players
+        for (int i = 0; i < a; i++) {
+            Utils.resetScreen();
+            System.out.println("Choose strategies for artificial player ".concat(Integer.toString(i)));
+
+            IdentityStrategy identityS = Utils.readStrategy(IdentityStrategy.class);
+            RespondStrategy respondS = Utils.readStrategy(RespondStrategy.class);
+            TurnStrategy turnS = Utils.readStrategy(TurnStrategy.class);
+
+            pManager.createArtificialPlayer(turnS, respondS, identityS);
+        }
 
         Utils.resetScreen();
         StringBuffer sb = new StringBuffer();
@@ -74,5 +85,14 @@ public class WitchHunt {
             sb.append(", ");
         }
         sb.deleteCharAt(sb.lastIndexOf(","));//remove useless last comma
+        System.out.println(sb);
+
+        //only for test
+        System.out.println("Press enter to continue.");
+        try {
+            System.in.read();//blocks until input data is available, i.e. until enter is pressed
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
