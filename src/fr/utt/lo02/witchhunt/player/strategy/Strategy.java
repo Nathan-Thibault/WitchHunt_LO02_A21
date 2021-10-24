256bf0bc7a39d50878ae39377db2b67aa60bc3a5
Nathan-Thibault <nathan.thibault@utt.fr>
2021-10-24 21:40:33 +0200

    feat: add enum for strategies type

--- src/fr/utt/lo02/witchhunt/player/strategy/Strategy.java
+++ src/fr/utt/lo02/witchhunt/player/strategy/Strategy.java
@@ -1,4 +1,29 @@
 package fr.utt.lo02.witchhunt.player.strategy;
 
+import fr.utt.lo02.witchhunt.player.strategy.identity.IdentityStrategy;
+import fr.utt.lo02.witchhunt.player.strategy.respond.RespondStrategy;
+import fr.utt.lo02.witchhunt.player.strategy.turn.TurnStrategy;
+
 public interface Strategy {
+    enum StrategyType {
+        IDENTITY(IdentityStrategy.class, "identity strategy"),
+        RESPOND(RespondStrategy.class, "respond strategy"),
+        TURN(TurnStrategy.class, "turn strategy");
+
+        private final Class<? extends Strategy> typeClass;
+        private final String name;
+
+        StrategyType(Class<? extends Strategy> typeClass, String name) {
+            this.typeClass = typeClass;
+            this.name = name;
+        }
+
+        public Class<? extends Strategy> getTypeClass() {
+            return typeClass;
+        }
+
+        public String getName() {
+            return name;
+        }
+    }
 }
