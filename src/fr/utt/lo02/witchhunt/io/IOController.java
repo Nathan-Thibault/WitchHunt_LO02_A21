a7fe4fdfc83590adcadda66b61d576c63a3531ad
Nathan <nathan.thibault@utt.fr>
2021-11-12 19:23:28 +0100

    feat: complete IOController

--- src/fr/utt/lo02/witchhunt/io/IOController.java
+++ src/fr/utt/lo02/witchhunt/io/IOController.java
@@ -1,23 +1,94 @@
 package fr.utt.lo02.witchhunt.io;
 
+import fr.utt.lo02.witchhunt.player.strategy.Strategy;
+
 import java.util.ArrayList;
 
 public class IOController {
 
     private static IOController instance;
 
-    private ArrayList<IOInterface> interfaces = new ArrayList<>();
+    private final ArrayList<IOInterface> interfaces = new ArrayList<>();
 
-    private IOController(){}
+    private volatile int readInt = -1;
+    private volatile Class<? extends Strategy> readStrategy = null;
 
-    public static IOController getInstance(){
-        if(instance == null){
+    private IOController() {
+        interfaces.add(new CommandLineInterface());
+    }
+
+    public static IOController getInstance() {
+        if (instance == null) {
             instance = new IOController();
         }
         return instance;
     }
 
-    public int readIntBetween(int min, int max){
+    public void setReadInt(int n) {
+        readInt = n;
+    }
 
+    public void setReadStrategy(Class<? extends Strategy> strategy) {
+        readStrategy = strategy;
+    }
+
+    public void clear() {
+        for (IOInterface ioInterface : interfaces) {
+            ioInterface.clear();
+        }
+    }
+
+    public void titleScreen() {
+        for (IOInterface ioInterface : interfaces) {
+            ioInterface.titleScreen();
+        }
+    }
+
+    public void printInfo(String msg) {
+        for (IOInterface ioInterface : interfaces) {
+            ioInterface.printInfo(msg);
+        }
+    }
+
+    public void printError(String msg) {
+        for (IOInterface ioInterface : interfaces) {
+            ioInterface.printError(msg);
+        }
+    }
+
+    public int readIntBetween(int min, int max) {
+        readInt = -1;
+
+        for (IOInterface ioInterface : interfaces) {
+            new Thread(() -> ioInterface.readIntBetween(min, max)).start();
+        }
+
+        while (readInt == -1) {
+            Thread.onSpinWait();
+        }
+
+        for (IOInterface ioInterface : interfaces) {
+            ioInterface.clear();
+        }
+
+        return readInt;
+    }
+
+    public Class<? extends Strategy> readStrategy(Strategy.StrategyType strategyType) {
+        readStrategy = null;
+
+        for (IOInterface ioInterface : interfaces) {
+            new Thread(() -> ioInterface.readStrategy(strategyType)).start();
+        }
+
+        while (readStrategy == null) {
+            Thread.onSpinWait();
+        }
+
+        for (IOInterface ioInterface : interfaces) {
+            ioInterface.clear();
+        }
+
+        return readStrategy;
     }
 }
