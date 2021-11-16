cdd565116bb88ce7748f8c67b23673b44594c90f
Nathan-Thibault <nathan.thibault@utt.fr>
2021-11-16 15:05:10 +0100

    feat: add a code to identities

--- src/fr/utt/lo02/witchhunt/Identity.java
+++ src/fr/utt/lo02/witchhunt/Identity.java
@@ -1,17 +1,31 @@
 package fr.utt.lo02.witchhunt;
 
 public enum Identity {
-    VILLAGER("Villager"),
-    WITCH("Witch");
+    VILLAGER("Villager", 1),
+    WITCH("Witch", 2);
 
     private final String name;
+    private final int code;
 
-    Identity(String name){
+    Identity(String name, int code) {
         this.name = name;
+        this.code = code;
+    }
+
+    public static Identity getByCode(int code) throws IllegalArgumentException {
+        for (Identity id : Identity.values()) {
+            if (id.getCode() == code)
+                return id;
+        }
+        throw new IllegalArgumentException("StrategyEnum getByCode: couldn't find a Identity with this code");
+    }
+
+    public int getCode() {
+        return code;
     }
 
     @Override
-    public String toString(){
+    public String toString() {
         return name;
     }
 }
