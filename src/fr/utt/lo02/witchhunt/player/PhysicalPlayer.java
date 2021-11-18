c9085bd81671f279c40c2572b80243b21a41e0d3
Nathan-Thibault <nathan.thibault@utt.fr>
2021-11-18 11:38:40 +0100

    feat: add PlayerAction enum

--- src/fr/utt/lo02/witchhunt/player/PhysicalPlayer.java
+++ src/fr/utt/lo02/witchhunt/player/PhysicalPlayer.java
@@ -3,6 +3,8 @@ package fr.utt.lo02.witchhunt.player;
 import fr.utt.lo02.witchhunt.io.IOController;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public final class PhysicalPlayer extends Player {
     public PhysicalPlayer(String name) {
@@ -47,5 +49,38 @@ public final class PhysicalPlayer extends Player {
 
         io.printInfo(name.concat(" choose a player from the list bellow:"));
         return io.readFromList(listOfPlayerNames);
+    }
+
+    enum PlayerAction {
+        ACCUSE("Accuse another player", false),
+        PLAY_HUNT("Play a hunt effect", false),
+        PLAY_WITCH("Play a witch effect", true),
+        REVEAL("Reveal your identity", true);
+
+        private final String description;
+        private final boolean respondAction;
+
+        PlayerAction(String description, boolean respondAction) {
+            this.description = description;
+            this.respondAction = respondAction;
+        }
+
+        public static List<PlayerAction> getTurnActions() {
+            return Arrays.stream(PlayerAction.values()).filter(pa -> !pa.isRespondAction()).toList();
+        }
+
+        public static List<PlayerAction> getRespondActions() {
+            return Arrays.stream(PlayerAction.values()).filter(PlayerAction::isRespondAction).toList();
+        }
+
+
+        public boolean isRespondAction() {
+            return respondAction;
+        }
+
+        @Override
+        public String toString() {
+            return description;
+        }
     }
 }
