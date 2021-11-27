86ce99db2c2ab9ea49d915a59a931463d55ceb6b
Nathan <nathan.thibault@utt.fr>
2021-11-27 14:43:39 +0100

    chore: remove unused import

--- src/fr/utt/lo02/witchhunt/card/effect/action/MustAccuse.java
+++ src/fr/utt/lo02/witchhunt/card/effect/action/MustAccuse.java
@@ -3,7 +3,6 @@ package fr.utt.lo02.witchhunt.card.effect.action;
 import fr.utt.lo02.witchhunt.RoundManager;
 import fr.utt.lo02.witchhunt.card.effect.CardEffect;
 import fr.utt.lo02.witchhunt.io.IOController;
-import fr.utt.lo02.witchhunt.player.Player;
 import fr.utt.lo02.witchhunt.player.PlayerManager;
 
 import java.util.HashMap;
